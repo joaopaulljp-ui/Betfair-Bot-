f5d90d5ee19ed3aa24809c5d2cd68c3bda6d08a1
joaopaulljp-ui <joaopaull.jp@gmail.com>
2026-04-20 15:54:40 -0300

    Refactor Alert model and remove unused classes

--- database.py
+++ database.py
@@ -1,75 +1,52 @@
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
-db = SQLAlchemy()
-
 class Alert(db.Model):
     __tablename__ = "alerts"
     
     id = db.Column(db.Integer, primary_key=True)
-    tipo = db.Column(db.String(50), nullable=False)
-    mensagem = db.Column(db.Text, nullable=False)
-    jogo = db.Column(db.String(200))
-    margem = db.Column(db.Float)
+    tipo = db.Column(db.String(50), nullable=False)  # arb, valor, etc
+     mensagem = db. Column(db.Text, nullable=False)
+    
+     # Informações do jogo # Informações do jogo
+      esporte = db. Column(db.String(100))  # Futebol, Basquete, etc Column(db.String(100))  # Futebol, Basquete, etc
+      jogo = db. Column(db.String(200))  # Time A vs Time B Column(db.String(200))  # Time A vs Time B
+     liga = db.Column(db.String(100))  # Campeonato Column(db.String(100))  # Campeonato
+    
+     # Arbitragem # Arbitragem
+     margem = db. Column(db.Float)
+    lucro_potencial = db.Column(db.Float)
+    
+     # Casas de apostas envolvidas # Casas de apostas envolvidas
     casa1 = db.Column(db.String(100))
     odd1 = db.Column(db.Float)
+    resultado1 = db.Column(db.String(100))  # Time A, Time B, Draw
+    link1 = db.Column(db.String(500))  # Link direto para aposta
+    
     casa2 = db.Column(db.String(100))
     odd2 = db.Column(db.Float)
+    resultado2 = db.Column(db.String(100))
+    link2 = db.Column(db.String(500))
+    
+    # Status
     data_criacao = db.Column(db.DateTime, default=datetime.utcnow)
-    enviado_telegram = db.Column(db.Boolean, default=False)
+     enviado_telegram = db. Column(db.Boolean, default=False)
     
     def to_dict(self):
         return {
             "id": self.id,
             "tipo": self.tipo,
-            "mensagem": self.mensagem,
-            "jogo": self.jogo,
-            "margem": self.margem,
-            "casa1": self.casa1,
-            "odd1": self.odd1,
-            "casa2": self.casa2,
-            "odd2": self.odd2,
-            "data": self.data_criacao.strftime("%d/%m/%Y %H:%M:%S") if self.data_criacao else "",
-            "enviado_telegram": self.enviado_telegram
-        }
-
-class Arbitrage(db.Model):
-    __tablename__ = "arbitrages"
-    
-    id = db.Column(db.Integer, primary_key=True)
-    jogo = db.Column(db.String(200), nullable=False)
-    odds = db.Column(db.JSON)
-    margem = db.Column(db.Float, nullable=False)
-    lucro_potencial = db.Column(db.Float)
-    data_deteccao = db.Column(db.DateTime, default=datetime.utcnow)
-    status = db.Column(db.String(50), default="pendente")
-    
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "jogo": self.jogo,
-            "odds": self.odds,
-            "margem": self.margem,
+             "esporte": self.esporte, "esporte": self.esporte,
+             "jogo": self.jogo, "jogo": self.jogo,
+            "liga": self.liga,
+             "mensagem": self.mensagem, "mensagem": self.mensagem,
+             "margem": self.margem, "margem": self.margem,
             "lucro": self.lucro_potencial,
-            "data": self.data_deteccao.strftime("%d/%m/%Y %H:%M:%S") if self.data_deteccao else "",
-            "status": self.status
-        }
-
-class Statistics(db.Model):
-    __tablename__ = "statistics"
-    
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.Date, default=datetime.utcnow)
-    arbitragens_detectadas = db.Column(db.Integer, default=0)
-    alertas_cartoes = db.Column(db.Integer, default=0)
-    alertas_valor = db.Column(db.Integer, default=0)
-    odds_erradas = db.Column(db.Integer, default=0)
-    
-    def to_dict(self):
-        return {
-            "data": self.data.strftime("%d/%m/%Y"),
-            "arbitragens": self.arbitragens_detectadas,
-            "cartoes": self.alertas_cartoes,
-            "valor": self.alertas_valor,
-            "odds_erradas": self.odds_erradas
+             "casa1": self.casa1, "casa1": self.casa1,
+            "odd1": self.odd1,
+             "resultado1": self.resultado1, "resultado1": self.resultado1,
+            "link1": self.link1,
+             "casa2": self.casa2, "casa2": self.casa2,
+            "odd2": self.odd2,
+             "resultado2": self.resultado2, "resultado2": self.resultado2,
+            "link2": self.link2,
+            "data": self.data_criacao.strftime("%d/%m/%Y %H:%M:%S") if self.data_criacao else "",
+             "enviado_telegram": self.enviado_telegram "enviado_telegram": self.enviado_telegram
         }
